5598795570a842de699fe933b5d57e6b0afbfad3
김군순 <rlarnstns@gmail.com>
2023-12-29 15:50:02 +0900

    Update MyBlogApplication.java

--- src/main/java/org/example/MyBlogApplication.java
+++ src/main/java/org/example/MyBlogApplication.java
@@ -8,7 +8,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing //자동 업데이트
 @SpringBootApplication // 스프링부트 사용에 필요한 기본 설정을 해줌
 public class MyBlogApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    public static void main(String[] args){
+        // 애플리케이션을 실행하는 메서드
+        // SpringApplication.run(메인클래스로 사용할 클래스, 커맨드 라인의 인수를 전달)
+        SpringApplication.run(MyBlogApplication.class, args);
+
     }
 }
